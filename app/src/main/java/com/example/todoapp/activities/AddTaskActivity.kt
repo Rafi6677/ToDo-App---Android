2775486642d6df41dda568999d570c3ba9bc93f4
Rafał Giemza <rafalgiemza6677@gmail.com>
2019-04-28 21:50:35 +0200

    AddTackActivity layout prototype

---
+++ app/src/main/java/com/example/todoapp/activities/AddTaskActivity.kt
@@ -0,0 +1,13 @@
+package com.example.todoapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.todoapp.R
+
+class AddTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
